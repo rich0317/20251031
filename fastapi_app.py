005200c4558ead39dd902003b73a4bf96cf004f8
rich0317 <rich66072003@hotmail.com>
2025-10-31 12:42:55 +0800

    style(快API): 修改根路径返回消息格式

--- fastapi_app.py
+++ fastapi_app.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return JSONResponse(content={"message": "Hello, FastAPI!"})
+    return JSONResponse(content={"message": "Hello! FastAPI!"})
 
 
 
