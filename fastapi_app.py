fb9da797217526e64ae66e0170b2fc780285a9b8
rich0317 <rich66072003@hotmail.com>
2025-10-31 12:38:28 +0800

    chore(快API): 移除未使用的测试端点

--- fastapi_app.py
+++ fastapi_app.py
@@ -9,5 +9,11 @@ def read_root():
     return JSONResponse(content={"message": "Hello, FastAPI!"})
 
 
+# 新增 /test1 節點
+@app.get("/test1")
+def test1():
+     return JSONResponse(content={"message": "This is test1 endpoint."})
+
+
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
